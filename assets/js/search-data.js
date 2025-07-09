9bd72fcc565fe995247632a70e90ce6afb9941c3
Nisanchhetri <46899689+Nisanchhetri@users.noreply.github.com>
2025-07-09 03:10:17 +0000

    Deploying to gh-pages from @ Nisanchhetri/nisanchhetri.github.io@8ed1e4361f02c78b52f413ef700a0edda0783ed6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -503,6 +503,13 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0003-3713-2085", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=LwzBlVQAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
